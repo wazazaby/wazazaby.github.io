93f938475b7d4b1ed2a618800c9b0aa385c10dae
Teddy <megareda1998@gmail.com>
2020-06-21 21:45:05 +0200

    Premier commit

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,23 @@
+<style>
+
+</style>
+
+<main>
+	<h1>Hello World!</h1>
+    <h2>{random}</h2>
+    <button on:click={setRandomEmoji}>Get new emoji</button>
+</main>
+
+<script>
+    const emojis = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘', '😗', '😙', '😚'];
+
+    const randomEmoji = () => {
+        return emojis[Math.floor(Math.random() * emojis.length)];
+    }
+
+    const setRandomEmoji = () => {
+        random = randomEmoji();
+    }
+    
+    let random = randomEmoji();
+</script>
